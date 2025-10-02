41f65a0886d06c841f2348c31da21c01968a0a92
misankin <misankin@techinline.ru>
2025-10-02 20:42:11 +0300

    6 jest

---
+++ 6_node/6_jest/src/utils/math.js
@@ -0,0 +1,10 @@
+// CommonJS
+// module.exports = {
+//     sum: (a, b) => a + b,
+//     multiply: (a, b) => a * b
+// };
+
+
+// ES6
+export function sum(a, b) { return a + b; }
+export function multiply(a, b) { return a * b; }
