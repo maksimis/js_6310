42314b7b6ced42ea441999c4f36eb2bd8d7f0cbd
Maksim <maksimis.reg@gmail.com>
2025-10-03 14:11:25 +0300

    es6

--- 6_node/0/src/index.js
+++ 6_node/0/src/index.js
@@ -1,7 +1,7 @@
 // CommonJS
-const math = require('./utils/math');
-console.log(math.sum(2, 3));
+// const math = require('./utils/math');
+// console.log(math.sum(2, 3));
 
 // ES6
-// import * as math from "./utils/math.js"
-// console.log(math.sum(2, 3));
+import * as math from "./utils/index.js"
+console.log(math.sum(2, 3));
