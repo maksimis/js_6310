5f26528f2340b699a62c24247aee79a36024051e
Maksim <maksimis.reg@gmail.com>
2025-10-03 11:33:28 +0300

    fix testes

--- 6_node/7_bot/tests/math.test.js
+++ 6_node/7_bot/tests/math.test.js
@@ -1,5 +1,6 @@
 // const math = require('../src/utils/math');
-import * as math from '../src/utils'
+import * as math from '../src/utils';
+import { describe, test, expect } from '@jest/globals';
 
 describe('Math module', () => {
     test('sums two numbers correctly', () => {
