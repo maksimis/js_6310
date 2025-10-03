be8ad34b1496259e72794445e81f55c58a654e03
Maksim <maksimis.reg@gmail.com>
2025-10-03 14:31:42 +0300

    bot

--- 6_node/0/src/index.js
+++ 6_node/0/src/index.js
@@ -1,7 +1,24 @@
-// CommonJS
-// const math = require('./utils/math');
-// console.log(math.sum(2, 3));
+import TelegramBot from 'node-telegram-bot-api'
+import dotenv from 'dotenv'
 
-// ES6
-import * as math from "./utils/index.js"
-console.log(math.sum(2, 3));
+dotenv.config();
+const token = process.env.TELEGRAM_BOT_TOKEN;
+
+// Создаем экземпляр бота
+const bot = new TelegramBot(token, { polling: true });
+
+// Обрабатываем команду /start
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Привет! Я твой первый бот!');
+});
+
+// Обрабатываем текстовые сообщения
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;    
+    if (text !== '/start') {
+        bot.sendMessage(chatId, `Вы сказали: "${text}"`);
+    }
+});
+console.log('Бот запущен...');
