b0d62f36ce672d40c2be0825d506b0c49e669a6f
misankin <misankin@techinline.ru>
2025-10-30 22:02:28 +0300

    3

---
+++ 9_react_ts/3/src/App.tsx
@@ -0,0 +1,34 @@
+import type { JSX } from 'react'
+
+import './App.css'
+import './App.css'
+import { Button } from './components/Button/Button'
+import { Card } from './components/Card/Card'
+
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>React TS Demo</h1>
+      </header>
+
+      <main className="app-main">
+        <Card title='Это заголовок' className='main-card'>
+          Это чилдрены
+          <Button>Кнопка</Button>
+        </Card>
+      </main>
+
+      <footer className="app-footer">
+        <div className="footer-content">
+          <Card>
+            <p>React TS Demo v1.0.0 - Демонстрационное приложение</p>
+            <p>React + TypeScript + Vite + Jest</p>
+          </Card>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default App
