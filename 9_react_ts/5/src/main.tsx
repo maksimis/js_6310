a2c71de1d9eab3b9949ef136374f3e8b7a04acb2
misankin <misankin@techinline.ru>
2025-10-31 09:24:53 +0300

    5

--- 9_react_ts/5/src/main.tsx
+++ 9_react_ts/5/src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
-
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root")
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+])
+
+createRoot(root!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
