cae548e582d471dea21dd98fbc91dff4b9ad6c4c
misankin <misankin@techinline.ru>
2025-11-14 10:28:18 +0300

    5

--- 10_router/5/src/pages/Login.tsx
+++ 10_router/5/src/pages/Login.tsx
@@ -1,7 +1,10 @@
+import LoginForm from "../components/LoginForm/LoginForm"
+
 const Login: React.FC = () => {
   return (
     <div>
       <h1>Login</h1>
+      <LoginForm></LoginForm>
     </div>
   )
 }
