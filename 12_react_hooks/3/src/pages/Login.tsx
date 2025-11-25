6eb118eb81a450323a116d5f0ce00c270930e5b2
misankin <misankin@techinline.ru>
2025-11-25 16:41:02 +0300

    3 start

---
+++ 12_react_hooks/3/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "../components/LoginForm/LoginForm"
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm></LoginForm>
+    </div>
+  )
+}
+
+export default Login
---
+++ 12_react_hooks/3/src/router.tsx
@@ -0,0 +1,44 @@
+// router.tsx
+import { createBrowserRouter, Navigate } from 'react-router'
+
+import App from './App'
+import About from './pages/About'
+import ErrorPage from './pages/ErrorPage'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import UserProfile from './pages/UserProfile'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'user/:userId',
+        element: <UserProfile />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: '*',
+        element: <Navigate to="/" replace />,
+      },
+    ],
+  },
+])
