4a6e810e98bbd7c358ed15933397ef171e559d31
misankin <misankin@techinline.ru>
2025-11-27 21:23:17 +0300

    4

--- 12_react_hooks/4/src/pages/Home.tsx
+++ 12_react_hooks/4/src/pages/Home.tsx
@@ -4,26 +4,46 @@ import { Button } from "../components/Button/Button"
 
 const Home: React.FC = () => {
   const [count, setCount] = useState(0)
-  
+  const [count2, setCount2] = useState(0)
+
   const increment = () => setCount(prev => prev + 1)
+  const increment2 = () => setCount2(prev => prev + 1)
 
   return (
     <div>
       <h1>Home</h1>
       <p>Count: {count}</p>
-      <Button onClick={increment}>+</Button>
-      <Child /> {/* Перерисовывается при каждом клике! */}
-      <ChildWithMemo /> {/* Не перерисовывается при каждом клике! */}
+      <p>Count2: {count2}</p>
+      <Button onClick={increment}>Count +</Button>
+      <Button onClick={increment2}>Count 2 +</Button>
+      <Child counter={count} /> {/* Перерисовывается при каждом клике! */}
+      <ChildWithMemo counter={count} /> {/* Не перерисовывается при каждом клике! */}
     </div>
   )
 }
 
-const ChildWithMemo = memo(() => <Child />)
+type ChildProps = {
+  counter: number
+}
 
-function Child() {
+const ChildWithMemo = memo(({ counter }: ChildProps) => <Child counter={counter} />)
+
+function Child({ counter }: ChildProps) {
   console.log('Child rendered!') // Вызывается каждый раз
 
-  return <div>Static Content</div>
+  const factorial = (num: number): number => {
+    if (num <= 1) {
+      return 1
+    } else if (num == 2) {
+      return 2
+    } else {
+      return factorial(num - 1) * factorial(num - 2)
+    }
+  }  
+
+  const fact =  factorial(counter)
+
+  return <div>Factorial {fact}</div>
 }
 
 export default Home
